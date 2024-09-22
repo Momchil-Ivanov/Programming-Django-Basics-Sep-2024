b2a544908bf95c7c85df28e2e792ca5fc5276a38
Momchil Ivanov <momchil2207@gmail.com>
2024-09-22 18:10:04 +0300

    Updated settings, URLs, and templates for the djangoIntroduction project

---
+++ djangoIntroduction/todo_app/urls.py
@@ -0,0 +1,5 @@
+from django.urls import path
+from djangoIntroduction.todo_app.views import index
+urlpatterns = [
+    path('', index),
+]
--- djangoIntroduction/wsgi.py
+++ djangoIntroduction/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for djangointroduction project.
+WSGI config for djangoIntroduction project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangointroduction.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangoIntroduction.settings')
 
 application = get_wsgi_application()
