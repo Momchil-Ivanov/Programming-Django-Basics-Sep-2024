3bcd6abcc176d553e3ed993effb46196a1e48355
Momchil Ivanov <88820552+Momchil-Ivanov@users.noreply.github.com>
2024-10-01 07:49:10 +0300

    Updated project with latest changes

--- petstagram/accounts/views.py
+++ petstagram/accounts/views.py
@@ -1,16 +1,21 @@
 from django.shortcuts import render
 
+
 def login(request):
     return render(request, 'accounts/login-page.html')
+
 
 def register(request):
     return render(request, 'accounts/register-page.html')
 
-def delete_page(request):
+
+def profile_delete(request, pk: int):
     return render(request, 'accounts/profile-delete-page.html')
 
-def profile_details(request):
+
+def profile_details(request, pk: int):
     return render(request, 'accounts/profile-details-page.html')
 
-def profile_edit(request):
+
+def profile_edit(request, pk: int):
     return render(request, 'accounts/profile-edit-page.html')
